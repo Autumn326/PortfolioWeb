d4c9db8bdf9e186c9e524458713253c13904040d
Dylan10pc <Dylanspc1204@hotmail.com>
2024-09-25 17:41:38 +0100

    made a few chnages and connected the web pages

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,1 @@
-// JavaScript to handle the scroll action
-document.querySelector('.scroll-indicator').addEventListener('click', function() {
-    document.getElementById('next-section').scrollIntoView({ behavior: 'smooth' });
-});
+
